101d5d6c7e38aaf773b085f45fdea2087d5d5281
Dimitar Kostov <xektoro@abv.bg>
2019-12-21 16:47:43 +0200

    Added index page and controller. Closes #8

---
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.example.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    // all this request will match to this Request Mapping
+    @RequestMapping({"", "/", "index", "index.hrml"})
+    public String index() {
+
+        // look for template called "index"
+        return "index";
+    }
+}
