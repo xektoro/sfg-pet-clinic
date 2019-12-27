53e58c2a326deacc6f6b7fac0ca0999a157750e6
Dimitar Kostov <xektoro@abv.bg>
2019-12-27 15:09:27 +0200

    Updated some Entities with JPA annotations. Closes #37

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Specialty.java
@@ -1,7 +1,14 @@
 package com.example.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity // is telling that this is going to be an entity persisted in the database
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Vet.java
@@ -1,11 +1,18 @@
 package com.example.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
     // we initialize it to avoid NPE
+    // By default many to many is lazy initialized
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+                             inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
