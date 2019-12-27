c6da2ce6408416f4e0aab70a784bad6b45b6f4c7
Dimitar Kostov <xektoro@abv.bg>
2019-12-27 15:52:24 +0200

    Added Spring Data JPA Service for Owners. Closes #40

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/repositories/OwnerRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 // because extending CrudRepository automatically we will have them in Spring Context
 // Spring Data JPA is going to provide us instances of these at runtime automatically
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
