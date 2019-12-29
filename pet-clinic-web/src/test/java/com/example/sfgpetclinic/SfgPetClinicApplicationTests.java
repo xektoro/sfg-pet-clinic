d37e685b981c5fbf8b787350262a2ade8e059376
Dimitar Kostov <xektoro@abv.bg>
2019-12-29 18:40:50 +0200

    JUnit 5. Closes #4

--- pet-clinic-web/src/test/java/com/example/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/example/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,8 +1,11 @@
 package com.example.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class) // in JUnit4 was @RunWith(SpringRunner.class)
 @SpringBootTest
 class SfgPetClinicApplicationTests {
 
