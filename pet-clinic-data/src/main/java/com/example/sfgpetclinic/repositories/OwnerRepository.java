bbd07998b721af4d6c240ef2bbc2faf3f11ca63d
Dimitar Kostov <xektoro@abv.bg>
2020-01-02 19:22:56 +0200

    Implemented Find Owner Functionality. Closes #54

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,9 +3,13 @@ package com.example.sfgpetclinic.repositories;
 import com.example.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 // because extending CrudRepository automatically we will have them in Spring Context
 // Spring Data JPA is going to provide us instances of these at runtime automatically
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/OwnerService.java
@@ -2,7 +2,11 @@ package com.example.sfgpetclinic.services;
 
 import com.example.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
