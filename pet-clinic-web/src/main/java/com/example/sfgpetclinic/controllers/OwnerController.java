397d1b5e7046fd7b4c41adc04a56c2529841b47b
Dimitar Kostov <xektoro@abv.bg>
2020-01-02 20:18:47 +0200

    Added Create and Update Owner Functionality. Closes #55

--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/OwnerController.java
@@ -7,14 +7,18 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+
+    private static final String VIEWS_OWNER_CREATE_OR_UPDATE_FORM = "owners/createOrUpdateOwnerForm";
 
     // hubavo e kato taka si inject-vame service-a, da go pravq final
     private final OwnerService ownerService;
@@ -80,5 +84,47 @@ public class OwnerController {
         ModelAndView mav = new ModelAndView("owners/ownerDetails");
         mav.addObject(ownerService.findById(ownerId));
         return mav;
+    }
+
+    @GetMapping("/new")
+    public String initCreationForm(Model model) {
+        model.addAttribute("owner", Owner.builder().build());
+        return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/new")
+    public String processCreationForm(@Valid Owner owner, BindingResult result) {
+        if (result.hasErrors()) {
+            return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+        } else {
+            Owner savedOwner =  ownerService.save(owner);
+            return "redirect:/owners/" + savedOwner.getId();
+        }
+    }
+
+    // if the String value in Mapping annotation (@PostMapping("/{ownerId}/edit"))
+    // matches the argument name of the @PathVariable annotation (@PathVariable Long ownerId)
+    // we do not need to explicitly type it again - "@PathVariable("ownerId")", could be just "@PathVariable"
+    @GetMapping("/{ownerId}/edit")
+    public String initUpdateOwnerForm(@PathVariable Long ownerId, Model model) {
+        model.addAttribute(ownerService.findById(ownerId));
+        return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+    }
+
+    // if the String value in Mapping annotation (@PostMapping("/{ownerId}/edit"))
+    // matches the argument name of the @PathVariable annotation (@PathVariable Long ownerId)
+    // we do not need to explicitly type it again - "@PathVariable("ownerId")", could be just "@PathVariable"
+    @PostMapping("/{ownerId}/edit")
+    public String processUpdateOwnerForm(@Valid Owner owner,
+                                         BindingResult result,
+                                         @PathVariable Long ownerId) {
+        if (result.hasErrors()) {
+            return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+        } else {
+            // we should explicitly set the id, because of the InitBinder (which is preventing the Model from getting the id)
+            owner.setId(ownerId);
+            Owner savedOwner = ownerService.save(owner);
+            return "redirect:/owners/" + savedOwner.getId();
+        }
     }
 }
